1799d354555d05f2d7e90fd179868a6f958742d9
Mathew_Steeve <mthwsteeve@gmail.com>
2024-07-19 19:02:46 +0530

    car generated and made it move

--- cars_generation.py
+++ cars_generation.py
@@ -5,3 +5,15 @@ import random
 class Car_Gen(Turtle):
     def __init__(self):
         super().__init__()
+        self.new_y = random.randint(-300, 300)
+        self.new_x = 320
+        self.new_seg()
+
+    def new_seg(self):
+        self.shape("square")
+        self.shapesize(1, 3)
+        self.penup()
+        self.goto(self.new_x, self.new_y)
+
+    def move_seg(self):
+        self.bk(20)
--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import time
 from turtle import Screen
 from turtle_creation import Turtle_Creation
+from cars_generation import Car_Gen
+
 screen = Screen()
 screen.setup(600, 600)
 screen.tracer(0)
+car = Car_Gen()
 turtle = Turtle_Creation()
 game_is_on = True
 
@@ -13,6 +16,7 @@ screen.onkey(turtle.go_up, "Up")
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+    car.move_seg()
 
 
 
